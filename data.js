41408684986b7753962e711f5332ece4682616e5
youzhenbaihu-stack <youzhenbaihu@gmail.com>
2026-02-23 16:41:19 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+// data.js
+const raceData = {
+  "中山": {
+    "芝": {
+      "1200m": {
+        "features": "スタートから最初のコーナーまでの距離が短い。外枠の馬はポジショニングがとりづらい上にすぐにコーナーに入るので全体のペースとしても上がりにくい。結果として内枠、先行馬が有利になりやすい。",
+        "stats": [
+          {"bracket": "1枠", "winRate": 10.7, "tan": 99, "fuku": 95},
+          {"bracket": "2枠", "winRate": 8.0,  "tan": 88, "fuku": 86},
+          {"bracket": "3枠", "winRate": 9.8,  "tan": 106,"fuku": 90},
+          {"bracket": "4枠", "winRate": 5.7,  "tan": 63, "fuku": 86},
+          {"bracket": "5枠", "winRate": 7.4,  "tan": 81, "fuku": 74},
+          {"bracket": "6枠", "winRate": 4.4,  "tan": 58, "fuku": 69},
+          {"bracket": "7枠", "winRate": 7.5,  "tan": 78, "fuku": 76},
+          {"bracket": "8枠", "winRate": 3.6,  "tan": 52, "fuku": 64}
+        ]
+      }
+    }
+  },
+  "京都": {
+    "ダート": {
+      "1200m": {
+        "features": "日本屈指の逃げ・先行有利コース。スタート後の3角の上り坂でペース緩む＋下り坂＋直線平坦。",
+        "stats": [
+          {"bracket": "1枠", "winRate": 6.7,  "tan": 75, "fuku": 74},
+          {"bracket": "2枠", "winRate": 4.0,  "tan": 47, "fuku": 84},
+          {"bracket": "3枠", "winRate": 7.2,  "tan": 96, "fuku": 70},
+          {"bracket": "4枠", "winRate": 4.3,  "tan": 61, "fuku": 73},
+          {"bracket": "5枠", "winRate": 3.8,  "tan": 45, "fuku": 65},
+          {"bracket": "6枠", "winRate": 7.8,  "tan": 89, "fuku": 87},
+          {"bracket": "7枠", "winRate": 11.1, "tan": 121,"fuku": 96},
+          {"bracket": "8枠", "winRate": 9.0,  "tan": 91, "fuku": 99}
+        ]
+      }
+    }
+  }
+};
